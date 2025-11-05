fbffc8e02b39883e943d20ab9f2b77ca94e31ada
Brandon Nguyen <t.brangu@gmail.com>
2025-11-04 23:05:16 -1000

    contacts table finished in 13 minutes

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,67 +1,40 @@
 import { getServerSession } from 'next-auth';
 import { Col, Container, Row } from 'react-bootstrap';
-import { loggedInProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
-import { Contact } from '@/lib/validationSchemas';
+import { loggedInProtectedPage } from '@/lib/page-protection';
+import { prisma } from '@/lib/prisma';
 import ContactCard from '@/components/ContactCard';
 
 const ListPage = async () => {
   const session = await getServerSession(authOptions);
-  loggedInProtectedPage(
-    session as {
-      user: { email: string; id: string; randomKey: string };
-    } | null,
-  );
+  loggedInProtectedPage(session as any);
 
-  const contacts: Contact[] = [
-    {
-      firstName: 'Philip',
-      lastName: 'Johnson',
-      address: 'POST 307, University of Hawaii',
-      image: 'https://github.com/philipmjohnson.png',
-      description:
-        'I am a Professor of Information and Computer Sciences at the University of Hawaii, '
-        + 'Director of the Collaborative Software Development Laboratory, and the CEO of '
-        + 'OpenPowerQuality.com.',
-    },
-    {
-      firstName: 'Henri',
-      lastName: 'Casanova',
-      address: 'POST 307, University of Hawaii',
-      image: 'https://avatars0.githubusercontent.com/u/7494478?s=460&v=4',
-      description:
-        'I am originally from France. I maintain a list of reports from my surf sessions. '
-        + 'I have proof that I ran the Hana relay with an actual Team.',
-    },
-    {
-      firstName: 'Kim',
-      lastName: 'Binsted',
-      address: 'POST 307, University of Hawaii',
-      image:
-        'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/kim_binsted-square-300x300.jpg',
-      description:
-        'Kim Binsted received her BSc in Physics at McGill (1991), and her PhD in Artificial '
-        + 'Intelligence from the University of Edinburgh (1996). Her thesis topic was the '
-        + 'computational modeling and generation of punning riddles, and her program, JAPE '
-        + '(Joke Analysis and Production Engine), generated puns such as '
-        + '"What do you call a Martian who drinks beer? An ale-ien!".',
-    },
-  ];
+  const owner = (session && session.user && session.user.email) || '';
+  const contacts = await prisma.contact.findMany({
+    where: { owner },
+    orderBy: [{ lastName: 'asc' }, { firstName: 'asc' }],
+  });
 
   return (
     <main>
       <Container id="list" fluid className="py-3">
         <Row>
           <Col>
-            <h1 className="text-center mb-4">Contacts</h1>
+            <h1 className="text-center mb-4">List Contacts</h1>
           </Col>
         </Row>
 
         <Row xs={1} md={2} lg={3} className="g-4">
-          {contacts.map((contact) => (
+          {contacts.map((c) => (
             <ContactCard
-              key={`Contact-${contact.firstName}-${contact.lastName}`}
-              contact={contact}
+              key={`Contact-${c.id}`}
+              contact={{
+                firstName: c.firstName,
+                lastName: c.lastName,
+                address: c.address,
+                image: c.image,
+                description: c.description,
+              }}
             />
           ))}
         </Row>
